2beac2b4a47e42884c902850cd0eccd55fc7d86c
showmiksadnan <showmik.sadnan@fau.de>
2022-09-04 22:49:52 +0200

    new

---
+++ reversenumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, r_number=0;
+    cout << "Number:";
+    cin >> number;
+
+    while (number != 0)
+    {
+        r_number = r_number*10; // multiply the r_number with 10
+        int lastDigit = number%10; 
+        r_number += lastDigit;
+        number = number/10;
+
+    }
+
+    cout << "Reverse: " << r_number <<endl;
+    
+}
